51c01015f77027abac723ff6771409311add546c
Fredrik <fredde.stromback@gmail.com>
2019-10-02 00:29:18 +0200

    Instances a ListView class

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 
 require_once('view/PageView.php');
+require_once('view/ListView.php');
 
 //MAKE SURE ERRORS ARE SHOWN... MIGHT WANT TO TURN THIS OFF ON A PUBLIC SERVER
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
 $pageView = new PageView();
+$listView = new ListView();
 
 $pageView->render();
