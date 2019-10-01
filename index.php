b0495796fa7420287e8ab43e9a8e83871fb9c1a4
Fredrik <fredde.stromback@gmail.com>
2019-10-02 00:38:48 +0200

    Remove error comment

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 require_once('view/PageView.php');
 require_once('view/ListView.php');
 
-//MAKE SURE ERRORS ARE SHOWN... MIGHT WANT TO TURN THIS OFF ON A PUBLIC SERVER
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
