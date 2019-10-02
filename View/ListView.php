89e85ba4ac8ecd8d687da6fe775264c4a92eaff1
Dennis Fredsson <dennis.fredsson@hotmail.com>
2019-10-02 14:46:27 +0200

    minor corrections

--- View/ListView.php
+++ View/ListView.php
@@ -2,22 +2,26 @@
 
 namespace View;
 
-class ListView {
+class ListView
+{
   // Detta är en kommentar
 
-  private $memberStorage; 
+  private $memberStorage;
 
-  public function __construct ($ms) {
+  public function __construct($ms)
+  {
     $this->memberStorage = $ms->getData();
   }
 
-  public function response () {
+  public function response()
+  {
     return '
     <p>This is from the ListView class, awesome!</p>
     ';
   }
 
-  public function createList() {
+  public function createList()
+  {
     $listString = '';
     for ($i = 0; $i < sizeof($this->memberStorage); $i++) {
       $name = $this->memberStorage[$i]->name;
