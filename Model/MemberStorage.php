5098c66a34a8f5ecae484149d517420f35008736
Dennis Fredsson <dennis.fredsson@hotmail.com>
2019-10-02 12:26:34 +0200

    all namespace folders now uses PascalCase

---
+++ Model/MemberStorage.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Model;
+
+class MemberStorage {
+  private $jsonFile;
+  private $phpObj;
+
+  public function __construct ($path) {
+    $this->jsonFile = file_get_contents($path, true);
+    $this->phpObj = json_decode($this->jsonFile);
+  }
+
+  public function getData () {
+    return $this->phpObj;
+  }
+}
---
+++ View/MemberView.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace View;
+
+class MemberView {
+
+  public function __construct () {
+
+  }
+
+  public function response () {
+    return '
+    <p>This is from the MemberView class, awesome!</p>
+    ';
+  }
+}
