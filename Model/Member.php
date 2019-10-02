cd53ad70ab919c0e457098ba70a20962e9fe367f
Dennis Fredsson <dennis.fredsson@hotmail.com>
2019-10-02 14:18:06 +0200

    Member.php implemented

--- Model/Member.php
+++ Model/Member.php
@@ -14,14 +14,60 @@ class Member
   { }
 
 
-  public function setID(): int
+  public function setID(int $ID): void
   {
-    return $this->ID;
+    $this->ID = $ID;
+  }
+
+  public function setName(string $name): void
+  {
+    $this->name = $name;
+  }
+
+  public function setPersonalNumber(string $personalNumber): void
+  {
+    $this->personalNumber = $personalNumber;
   }
 
 
   public function getID(): int
   {
     return $this->ID;
+  }
+
+  public function getName(): string
+  {
+    return $this->name;
+  }
+
+  public function getPersonalNumber(): string
+  {
+    return $this->personalNumber;
+  }
+
+  public function getBoatCount(): int
+  {
+    return count($this->boats);
+  }
+
+
+  public function addBoat(\Model\Boat $newBoat): void
+  {
+    array_push($this->boats, $newBoat);
+  }
+
+  public function findBoatByID(int $ID): \Model\Boat
+  {
+    foreach ($this->boats as $boat) {
+      if ($boat->id == $ID) {
+        return $boat;
+      }
+    }
+  }
+
+  public function removeBoat(int $ID): void
+  {
+    $boatToRemove = $this->findBoatByID($ID);
+    array_splice($this->boats, $boatToRemove);
   }
 }
