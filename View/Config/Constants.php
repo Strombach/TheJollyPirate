0d309874d763ef7c416c69fba937ed50bb55f37e
markusohlen <mo223dg@student.lnu.se>
2019-11-12 19:00:36 +0100

    Pushing test urls

--- View/Config/Constants.php
+++ View/Config/Constants.php
@@ -4,11 +4,11 @@ namespace View\Config;
 
 class Constants
 {
-  public const memberURL = "member";
-  public const deleteURL = "delete";
-  public const verboseURL = "verbose";
-  public const compactURL = "compact";
-  public const deleteBoatURL = "deleteboat";
+  public const memberURL = "TESTmember";
+  public const deleteURL = "TESTdelete";
+  public const verboseURL = "TESTverbose";
+  public const compactURL = "TESTcompact";
+  public const deleteBoatURL = "TESTdeleteboat";
   public const editMemberURL = "editmember";
   public const editBoatURL = "editboat";
 }
